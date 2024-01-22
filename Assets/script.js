11379c9c8378304186aabbfa74ec718e25f8f8e6
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-01-22 16:13:43 -0500

    updated some html and added vars and questions template in js

---
+++ Assets/script.js
@@ -0,0 +1,63 @@
+    var startBtn = document.getElementById("startBtn");
+    var time = 120;
+    var timeRemaining = document.getElementById("timeRemaining");
+    var headerBox =  document.getElementById("headerBox");
+    var quizBox = document.getElementById("quizBox");
+    var questionHeader = document.getElementById("questionHeader");
+    var choiceA = document.getElementById("choiceA");
+    var choiceB = document.getElementById("choiceB");
+    var choiceC = document.getElementById("choiceC");
+    var choiceD = document.getElementById("choiceD");
+    var correctAnswer = document.getElementById("correctAnswer");    
+
+    var questionsArray = [
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        }, 
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        }, 
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        },
+        {
+            question: "",
+            answerChoice: [""],
+            correctAnswer: 
+        }];
